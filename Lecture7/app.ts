77ac90f2022dcf96bb9e75d5a120cc47ec5c95da
Umar Ejaz <umar.senpaiiii@gmail.com>
2025-05-07 02:55:46 -0700

    v8

---
+++ Lecture7/app.ts
@@ -0,0 +1,55 @@
+// // // lets learn generics
+
+// // var fruits: Array<string | number> = ["apple", "banana", "orange"];
+// // fruits.push("grapes");
+// // fruits.push(10);
+// // console.log(fruits);
+
+// // type person = {
+// //   name: string;
+// //   age: number;
+// // };
+// // var person: Array<person> = [
+// //   {
+// //     name: "John",
+// //     age: 30,
+// //   },
+// // ];
+// // person.push({
+// //   name: "Jane",
+// //   age: 25,
+// // });
+// // console.log(person);
+
+// // function merge<T extends object, U extends object>(objA: T, objB: U): {} {
+// //   return { ...objA, ...objB };
+// // }
+// // var result = merge({ name: "John", role: "admin" }, { age: 30 });
+
+// // console.log(result);
+
+// interface responseData<T extends object> {
+//   data: T;
+//   length: number;
+// }
+// type person = {
+//   name: string;
+//   age: number;
+// };
+
+// var person: responseData<person> = {
+//   data: {
+//     name: "John",
+//     age: 30,
+//   },
+//   length: 2,
+// };
+// console.log(person);
+
+function getFirst<T>(arr: T[]): T {
+  return arr[0];
+}
+var numberARR = getFirst([1, 2, 3, 4]);
+var stringARR = getFirst(["a", "b", "c", "d"]);
+
+console.log(numberARR, stringARR);
